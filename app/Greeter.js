60f36bb3734b6cc7467bbf8220391a80d1c6d96b
haoniansheng <haoniansheng1@163.com>
2017-07-15 18:52:00 +0800

    Add files via upload

---
+++ app/Greeter.js
@@ -0,0 +1,5 @@
+module.exports = function() {
+  var greet = document.createElement('div');
+  greet.textContent = "Hi ther11111232134213e1111221321 and greetings!";
+  return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,8 @@
+//main.js
+//main.js
+
+import Greeter from './Greeter';
+
+import './main.css';//使用require导入css文件
+
+//render(<Greeter/>, document.getElementById('root'));
